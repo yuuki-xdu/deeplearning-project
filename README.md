9fe8a219a2d2c2b3d2c6c88a26c7f141de3136ec
yuuki <ycwang0821@qq.com>
2024-10-21 11:04:48 +0800

    111

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-这是在该项目master分支下的readme，mark一下
 # deeplearning-project
 pdw radar identification by keras
+20241021 记录
+考虑在模型中加入开头的正则化层，末尾加入dropout层来改善过拟合现象
+
+
+20241022
--- yModel1.py
+++ yModel1.py
@@ -7,32 +7,29 @@ from keras.api.utils import to_categorical,plot_model
 from keras.api.callbacks import ModelCheckpoint, EarlyStopping,LearningRateScheduler
 from keras.api.optimizers import Adam
 from sklearn.preprocessing import StandardScaler
+'''
+读取数据并处理
+'''
 #读取CSV文件
-#这里的csv文件由matlab生成，为5n行10列的矩阵，前9列为脉冲描述字，10列为标签0-4，共5种
+#这里的csv文件由matlab仿真生成，为5n行10列的矩阵，前9列为脉冲描述字，10列为标签0-4，共5种
 data = pd.read_csv('signal_data_with_labels.csv', header=None)
-
-
-scaler = StandardScaler()
-
-'''
-# 数据分为特征(X)和标签(y)
-X = data.iloc[:, :-1].values  # 前3列是特征(载频、脉宽、到达时间差)
-y = data.iloc[:, -1].values   # 最后一列是标签(0到5)
-# 只提取4、5、9列作为特征,以及最后一列作为标签
-'''
-
 X = data.iloc[:, [3, 4, 8]].values  # 第4列(索引为3)、第5列(索引为4)、第9列(索引为8)
 y = data.iloc[:, -1].values         # 最后一列(标签)
-
 # 将标签进行独热编码（one-hot encoding）
 y = to_categorical(y, num_classes=5)
 
+
+scaler = StandardScaler()
 # 划分训练集和测试集
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 # 使用Adam优化器并设置学习率为0.001
 optimizer = Adam(learning_rate=0.00025)
+
+'''
+模型创建
+'''
 
 # 创建Keras模型
 model = Sequential()
